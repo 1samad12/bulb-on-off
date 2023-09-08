b60afe86a06d27f3c054af0d5fdda6e24f2b02fa
1samad12 <samad123450099@gmail.com>
2023-09-07 20:46:27 -0700

    bulb on/off

---
+++ app.js
@@ -0,0 +1,7 @@
+function bulbon(){
+  document.getElementById('img').src ='https://img.freepik.com/free-vector/realistic-lit-light-bulb-isolated_1284-41774.jpg?w=2000'
+  
+}
+function bulboff(){
+  document.getElementById('img').src ='https://www.lampshoponline.com/media/catalog/product/2/4/24v_gls.png?optimize=high&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700'
+}
